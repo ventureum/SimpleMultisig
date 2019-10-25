adb929e0954f643009295646991fb7150ba44e10
Timothy Wang <timothywangdev@gmail.com>
2019-10-25 19:28:37 +0000

    Resolve #1, #2, #3
    Resolve #1 Support createWallet function
    Resolve #2 Support transferETH
    Resolve #3 Support transferERC20

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,10 @@
-var MyContract = artifacts.require("./SimpleMultiSig.sol");
+var SimpleMultiSig = artifacts.require('./SimpleMultiSig.sol')
 
 module.exports = function(deployer) {
   // deployment steps
-  deployer.deploy(MyContract, 2, ["0x5505Ab49B83240A617FdE18d4F1DAAb5AC691E58", "0x7B00aE36C7485B678Fe945c2DD9349Eb5Baf7b6B"], 0);
-};
+  // rinkeby
+  deployer.deploy(SimpleMultiSig, 4)
+
+  // mainnet
+  // deployer.deploy(SimpleMultiSig, 1)
+}
--- truffle.js
+++ truffle.js
@@ -1,9 +1,57 @@
+// babel does not transpile js in node_modules, we need to ignore them
+require('babel-register')({
+  ignore: /node_modules\/(?!openzeppelin-solidity\/test\/helpers)/
+})
+require('babel-polyfill')
+
+const PrivateKeyProvider = require('truffle-privatekey-provider')
+const fs = require('fs')
+
+let privateKey = ''
+
+if (fs.existsSync('privateKey.txt')) {
+  privateKey = fs
+    .readFileSync('privateKey.txt')
+    .toString()
+    .split('\n')[0]
+}
+
 module.exports = {
+  quiet: false,
   networks: {
     development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
+      host: 'localhost',
+      port: 7545,
+      network_id: '*' // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new PrivateKeyProvider(
+          privateKey,
+          'https://rinkeby.infura.io/v3/9775236247814824bc231e65b1d4972a'
+        )
+      },
+      network_id: '4',
+      gas: 6000000,
+      gasPrice: 20000000000
+    },
+    mainnet: {
+      provider: function() {
+        return new PrivateKeyProvider(
+          privateKey,
+          'https://mainnet.infura.io/v3/9775236247814824bc231e65b1d4972a'
+        )
+      },
+      network_id: '1',
+      gas: 7000000,
+      gasPrice: 10000000000
     }
+  },
+  mocha: {
+    reporter: 'eth-gas-reporter',
+    reporterOptions: {
+      currency: 'USD',
+      gasPrice: 20
+    } // See options below
   }
-};
+}
